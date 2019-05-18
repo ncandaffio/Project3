2e24106fa48fcc1fc3256f3ac4fad73c64275c93
blueskies <desireec.ferrer@gmail.com>
2019-05-17 22:55:14 -0400

    Project Pre

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import pymongo
+from mongo_to_geojson import mongo_to_geojson 
+#################################################
+# Database Setup
+#################################################
+
+
+# app = Flask(__name__)
+
+mongo_uri = 'mongodb://localhost/gisdata'
+client = pymongo.MongoClient(mongo_uri)
+db = client.election 
+collection = 'db.test'
+output_geojson =  'test.json'
+# query = {"POP": {"$gte": 100000}}
+
+# @app.route("/2016")
+# def election2016():
+try:  
+    mongo_to_geojson(mongo_uri,collection,output_geojson)  
+except:  
+    print('mongo_to_geojson no worky')
+
+#################################################
+# GEOJSON
+#################################################
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
